d54a305f91fcbb89f821a3aaf3c4932a5ecde177
Pravin Kumbhare <pravinkumbhare94@gmail.com>
2023-02-03 20:37:11 +0530

    add counter-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="jumbotron text-center">
+        <h1>Counter</h1>
+        <h1>{counter}</h1>
+         
+      </div>
+      <div class="container">
+        
+        <div className="row">
+          <div className="col-sm-4 text-center">
+            <button type="button" className="btn btn-primary" onClick={() => setCounter(counter+1)}>Increment</button>
+          </div>
+          <div className="col-sm-4 text-center">
+            <button type="button" className="btn btn-primary" onClick={() => setCounter(counter-1)} disabled={counter ===0}>Decrement</button>
+          </div>
+          <div className="col-sm-4 text-center">
+            <button type="button" className="btn btn-danger" onClick={() => setCounter(0)}>Reset</button>
+          
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
